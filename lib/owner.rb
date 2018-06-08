47068a94b74ea47b1f6e1da7577e030e05f0ab00
David Hopkins <dlhopkins2@gmail.com>
2018-06-08 17:07:51 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -38,12 +38,12 @@ class Owner
     pets[:fishes]<< fish
   end
 
-  def buy_cat
+  def buy_cat(name)
     cat = Cat.new(name)
     pets[:cats]<< cat
   end
 
-  def buy_dog
+  def buy_dog(name)
     dog = Dog.new(name)
     pets[:dogs]<< dog
   end

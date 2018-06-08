2fde52f868b680e501f6d676e94345326a3e1e39
David Hopkins <dlhopkins2@gmail.com>
2018-06-08 16:49:10 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,72 @@
 class Owner
-  # code goes here
+  attr_accessor :pets
+  attr_reader :species
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @pets = {fishes: [], cats: [], dogs: []}
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    self.all.size
+  end
+
+  def self.reset_all
+    self.all.clear
+  end
+
+  def species(species)
+
+  end
+
+  def say_species
+
+  end
+
+  def name(name)
+
+  end
+
+  def pets
+
+  end
+
+  def buy_fish
+    self.fish.new(name)
+  end
+
+  def buy_cat
+
+  end
+
+  def buy_dog
+
+  end
+
+  def walk_dogs
+
+  end
+
+  def play_with_cats
+
+  end
+
+  def feed_fish
+
+  end
+
+  def sell_pets
+
+  end
+
+  def list_pets
+
+  end
+
 end
